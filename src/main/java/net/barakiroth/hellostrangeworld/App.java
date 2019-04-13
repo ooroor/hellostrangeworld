61a07fb559a8d3d86af1d5e7e782aaaa1e2f7ee4
Øyvind Roth <oyvind@barakiroth.net>
2019-04-13 23:48:02 +0200

    Create a project originating from by Maven quickstart archetype with versions bumped to latest.

---
+++ src/main/java/net/barakiroth/hellostrangeworld/App.java
@@ -0,0 +1,9 @@
+package net.barakiroth.hellostrangeworld;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello strange world!" );
+    }
+}
