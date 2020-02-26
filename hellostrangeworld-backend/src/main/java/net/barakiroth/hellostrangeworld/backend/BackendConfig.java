7c402cf453c65ba45f260723fe5441ae18d77a57
Øyvind Roth <oyvind@barakiroth.net>
2020-02-27 00:09:44 +0100

    Make the specific config files more testable

--- hellostrangeworld-backend/src/main/java/net/barakiroth/hellostrangeworld/backend/BackendConfig.java
+++ hellostrangeworld-backend/src/main/java/net/barakiroth/hellostrangeworld/backend/BackendConfig.java
@@ -8,37 +8,60 @@ import net.barakiroth.hellostrangeworld.common.AbstractConfig;
 import net.barakiroth.hellostrangeworld.common.infrastructure.servletcontainer.IJettyManagerConfig;
 import net.barakiroth.hellostrangeworld.common.infrastructure.servletcontainer.JettyManagerConfig;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public class BackendConfig extends AbstractConfig implements IBackendConfig {
 
-	private static final Logger log = LoggerFactory.getLogger(BackendConfig.class);
-	private static final Logger enteringMethodHeaderLogger = LoggerFactory.getLogger("EnteringMethodHeader");
-	private static final Logger leavingMethodHeaderLogger = LoggerFactory.getLogger("LeavingMethodHeader");
+  @Getter(AccessLevel.PUBLIC)
+  private static final IBackendConfig singletonInstance = new BackendConfig();
 
-	@Getter(AccessLevel.PUBLIC)
-	private static final IBackendConfig singletonInstance = new BackendConfig();
+  private IJettyManagerConfig jettyManagerConfig;
+  private JettyManager        jettyManager;
+  private PrometheusConfig    prometheusConfig;
 
-	@Getter(AccessLevel.PUBLIC)
-	private final IJettyManagerConfig jettyManagerConfig;
+  private BackendConfig() {
+    super();
+  }
 
-	@Getter(AccessLevel.PUBLIC)
-	private final JettyManager jettyManager;
+  private void setJettyManagerConfig(final IJettyManagerConfig jettyManagerConfig) {
+    this.jettyManagerConfig = jettyManagerConfig;
+  }
 
-	@Getter(AccessLevel.PUBLIC)
-	private final PrometheusConfig prometheusConfig;
+  @Override
+  public IJettyManagerConfig getJettyManagerConfig() {
+    if (this.jettyManagerConfig == null) {
+      // TODO: Should call the getter, not the creator:
+      final IJettyManagerConfig jettyManagerConfig =
+          JettyManagerConfig.createSingletonInstance(this);
+      setJettyManagerConfig(jettyManagerConfig);
+    }
+    return this.jettyManagerConfig;
+  }
 
-	private BackendConfig() {
+  private void setJettyManager(final JettyManager jettyManager) {
+    this.jettyManager = jettyManager;
+  }
 
-		super();
+  @Override
+  public JettyManager getJettyManager() {
+    if (this.jettyManager == null) {
+      // TODO: Should call the getter, not the creator:
+      final JettyManager jettyManager =
+          JettyManager.getSingletonInstance(this, JerseyApplication.class.getName());
+      setJettyManager(jettyManager);
+    }
+    return this.jettyManager;
+  }
 
-		enteringMethodHeaderLogger.debug(null);
+  private void setPrometheusConfig(final PrometheusConfig prometheusConfig) {
+    this.prometheusConfig = prometheusConfig;
+  }
 
-		this.jettyManagerConfig = JettyManagerConfig.createSingletonInstance(this);
-		this.jettyManager = JettyManager.getSingletonInstance(this, JerseyApplication.class.getName());
-		this.prometheusConfig = PrometheusConfig.createSingletonInstance(this);
-
-		leavingMethodHeaderLogger.debug(null);
-	}
+  @Override
+  public PrometheusConfig getPrometheusConfig() {
+    if (this.prometheusConfig == null) {
+      // TODO: Should call the getter, not the creator:
+      final PrometheusConfig prometheusConfig = PrometheusConfig.createSingletonInstance(this);
+      setPrometheusConfig(prometheusConfig);
+    }
+    return this.prometheusConfig;
+  }
 }
--- hellostrangeworld-farbackend/src/main/java/net/barakiroth/hellostrangeworld/farbackend/FarBackendConfig.java
+++ hellostrangeworld-farbackend/src/main/java/net/barakiroth/hellostrangeworld/farbackend/FarBackendConfig.java
@@ -10,105 +10,107 @@ import net.barakiroth.hellostrangeworld.farbackend.infrastructure.database.Datab
 import net.barakiroth.hellostrangeworld.farbackend.infrastructure.database.DatabaseConfig;
 import net.barakiroth.hellostrangeworld.farbackend.infrastructure.prometheus.PrometheusConfig;
 import net.barakiroth.hellostrangeworld.farbackend.infrastructure.servletcontainer.JettyManager;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class FarBackendConfig extends AbstractConfig implements IFarBackendConfig {
 
-	private static final Logger log = LoggerFactory.getLogger(FarBackendConfig.class);
-	private static final Logger enteringMethodHeaderLogger = LoggerFactory.getLogger("EnteringMethodHeader");
-	private static final Logger leavingMethodHeaderLogger = LoggerFactory.getLogger("LeavingMethodHeader");
+  @Getter(AccessLevel.PUBLIC)
+  private static final IFarBackendConfig singletonInstance = new FarBackendConfig();
 
-	@Getter(AccessLevel.PUBLIC)
-	private static final IFarBackendConfig singletonInstance = new FarBackendConfig();
-	
-	private IJettyManagerConfig jettyManagerConfig;
-	private JettyManager        jettyManager;
-	private DatabaseConfig      databaseConfig;
-	private Database            database;
-	private Repository          repository;
-	private PrometheusConfig    prometheusConfig;
+  private IJettyManagerConfig jettyManagerConfig;
+  private JettyManager jettyManager;
+  private DatabaseConfig databaseConfig;
+  private Database database;
+  private Repository repository;
+  private PrometheusConfig prometheusConfig;
 
-	private FarBackendConfig() {
-		super();
-	}
-	
-	private void setJettyManagerConfig(final IJettyManagerConfig jettyManagerConfig) {
-		this.jettyManagerConfig = jettyManagerConfig;
-	}
+  private FarBackendConfig() {
+    super();
+  }
 
-	public IJettyManagerConfig getJettyManagerConfig() {
-		if (this.jettyManagerConfig == null) {
-			// TODO: Should call the getter, not the creator:
-			final IJettyManagerConfig jettyManagerConfig = JettyManagerConfig.createSingletonInstance(this);
-			setJettyManagerConfig(jettyManagerConfig);
-		}
-		return this.jettyManagerConfig;
-	}
-	
-	private void setJettyManager(final JettyManager jettyManager) {
-		this.jettyManager = jettyManager;
-	}
+  private void setJettyManagerConfig(final IJettyManagerConfig jettyManagerConfig) {
+    this.jettyManagerConfig = jettyManagerConfig;
+  }
 
-	public JettyManager getJettyManager() {
-		if (this.jettyManager == null) {
-			// TODO: Should call the getter, not the creator:
-			final JettyManager jettyManager = JettyManager.getSingletonInstance(this, JerseyApplication.class.getName());
-			setJettyManager(jettyManager);
-		}
-		return this.jettyManager;
-	}
-	
-	private void setDatabaseConfig(final DatabaseConfig databaseConfig) {
-		this.databaseConfig = databaseConfig;
-	}
+  @Override
+  public IJettyManagerConfig getJettyManagerConfig() {
+    if (this.jettyManagerConfig == null) {
+      // TODO: Should call the getter, not the creator:
+      final IJettyManagerConfig jettyManagerConfig =
+          JettyManagerConfig.createSingletonInstance(this);
+      setJettyManagerConfig(jettyManagerConfig);
+    }
+    return this.jettyManagerConfig;
+  }
 
-	public DatabaseConfig getDatabaseConfig() {
-		if (this.databaseConfig == null) {
-			// TODO: Should call the getter, not the constructor:
-			final DatabaseConfig databaseConfig = new DatabaseConfig(this);
-			setDatabaseConfig(databaseConfig);
-		}
-		return this.databaseConfig;
-	}
-	
-	private void setDatabase(final Database database) {
-		this.database = database;
-	}
+  private void setJettyManager(final JettyManager jettyManager) {
+    this.jettyManager = jettyManager;
+  }
 
-	public Database getDatabase() {
-		if (this.database == null) {
-			// TODO: Should call the getter, not the constructor:
-			final Database database = new Database(this);
-			setDatabase(database);
-		}
-		return this.database;
-	}
-	
-	private void setRepository(final Repository repository) {
-		this.repository = repository;
-	}
+  @Override
+  public JettyManager getJettyManager() {
+    if (this.jettyManager == null) {
+      // TODO: Should call the getter, not the creator:
+      final JettyManager jettyManager =
+          JettyManager.getSingletonInstance(this, JerseyApplication.class.getName());
+      setJettyManager(jettyManager);
+    }
+    return this.jettyManager;
+  }
 
-	public Repository getRepository() {
-		if (this.repository == null) {
-			// TODO: Should call the getter, not the constructor:
-			final Repository repository = new Repository(this);
-			setRepository(repository);
-		}
-		return this.repository;
-	}
-	
-	private void setPrometheusConfig(final PrometheusConfig prometheusConfig) {
-		this.prometheusConfig = prometheusConfig;
-	}
+  private void setPrometheusConfig(final PrometheusConfig prometheusConfig) {
+    this.prometheusConfig = prometheusConfig;
+  }
 
-	public PrometheusConfig getPrometheusConfig() {
-		if (this.prometheusConfig == null) {
-			// TODO: Should call the getter, not the creator:
-			final PrometheusConfig prometheusConfig = PrometheusConfig.createSingletonInstance(this);
-			setPrometheusConfig(prometheusConfig);
-		}
-		return this.prometheusConfig;
-	}
-	
+  @Override
+  public PrometheusConfig getPrometheusConfig() {
+    if (this.prometheusConfig == null) {
+      // TODO: Should call the getter, not the creator:
+      final PrometheusConfig prometheusConfig = PrometheusConfig.createSingletonInstance(this);
+      setPrometheusConfig(prometheusConfig);
+    }
+    return this.prometheusConfig;
+  }
+
+  private void setDatabaseConfig(final DatabaseConfig databaseConfig) {
+    this.databaseConfig = databaseConfig;
+  }
+
+  @Override
+  public DatabaseConfig getDatabaseConfig() {
+    if (this.databaseConfig == null) {
+      // TODO: Should call the getter, not the constructor:
+      final DatabaseConfig databaseConfig = new DatabaseConfig(this);
+      setDatabaseConfig(databaseConfig);
+    }
+    return this.databaseConfig;
+  }
+
+  private void setDatabase(final Database database) {
+    this.database = database;
+  }
+
+  @Override
+  public Database getDatabase() {
+    if (this.database == null) {
+      // TODO: Should call the getter, not the constructor:
+      final Database database = new Database(this);
+      setDatabase(database);
+    }
+    return this.database;
+  }
+
+  private void setRepository(final Repository repository) {
+    this.repository = repository;
+  }
+
+  @Override
+  public Repository getRepository() {
+    if (this.repository == null) {
+      // TODO: Should call the getter, not the constructor:
+      final Repository repository = new Repository(this);
+      setRepository(repository);
+    }
+    return this.repository;
+  }
+
 }
